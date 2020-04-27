b4f420899938a9d7b806428cb36cb768c6c89976
Ruslan <ttracer64@gmail.com>
2020-04-28 00:32:30 +0300

    Initial

---
+++ index.js
@@ -0,0 +1,11 @@
+const handler = require('serve-handler');
+const http = require('http');
+
+// serve
+const server = http.createServer((request, response) => {
+    // You pass two more arguments for config and middleware
+    // More details here: https://github.com/zeit/serve-handler#options
+    return handler(request, response);
+});
+
+server.listen(80);
